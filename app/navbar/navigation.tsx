2d867bf686cd2b006310a9be1b13f1916841753f
yurannhassengo <yurannhassengo@gmail.com>
2024-10-02 14:22:57 -0700

    add navigationBar

--- app/navbar/navigation.tsx
+++ app/navbar/navigation.tsx
@@ -1,11 +1,151 @@
+'use client'
+
 import React from 'react'
 
-const NavigationBar = () => {
-  return (
-    <div>
-        navigationBar
-    </div>
-  )
-}
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    NavigationMenuViewport,
+  } from "@/components/ui/navigation-menu"
 
-export default NavigationBar
+
+  import logo from "../images/logo/logo.png"
+import Link from 'next/link'
+import Logo from './logo'
+import { cn } from '@/lib/utils'
+
+  const components : {title:string,description:string}[] =[
+
+    {
+        title:"Frozen",
+
+        description:"Lorem ipsum dolor sit, amet consectetur adipisicing elit." 
+ 
+    },
+    {
+        title:"Tuscany",
+
+        description:"Lorem ipsum dolor sit, amet consectetur adipisicing elit." 
+ 
+    },
+    {
+        title:"Swift",
+
+        description:"Lorem ipsum dolor sit, amet consectetur adipisicing elit." 
+ 
+    },
+    {
+        title:"Mint",
+
+        description:"Lorem ipsum dolor sit, amet consectetur adipisicing elit." 
+ 
+    },
+    {
+        title:"Tide",
+
+        description:"Lorem ipsum dolor sit, amet consectetur adipisicing elit." 
+ 
+    },
+    {
+        title:"Amber",
+
+        description:"Lorem ipsum dolor sit, amet consectetur adipisicing elit." 
+ 
+    },
+  ]
+  
+
+  export default function NavigationBar() {
+    return (
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                <li className="row-span-3">
+                  <NavigationMenuLink asChild>
+                    <a
+                      className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                      href="/"
+                    >
+                      <Logo />
+                      <div className="mb-2 mt-4 text-lg font-medium">
+                        shadcn/ui
+                      </div>
+                      <p className="text-sm leading-tight text-muted-foreground">
+                        Beautifully designed components built with Radix UI and
+                        Tailwind CSS.
+                      </p>
+                    </a>
+                  </NavigationMenuLink>
+                </li>
+                <ListItem href="/docs" title="Introduction">
+                  Re-usable components built using Radix UI and Tailwind CSS.
+                </ListItem>
+                <ListItem href="/docs/installation" title="Installation">
+                  How to install dependencies and structure your app.
+                </ListItem>
+                <ListItem href="/docs/primitives/typography" title="Typography">
+                  Styles for headings, paragraphs, lists...etc
+                </ListItem>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Components</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                {components.map((component) => (
+                  <ListItem
+                    key={component.title}
+                    title={component.title}
+                  >
+                    {component.description}
+                  </ListItem>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/docs" legacyBehavior passHref>
+              <NavigationMenuLink >
+                Documentation
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    )
+  }
+
+  const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  )
+})
+ListItem.displayName = "ListItem"
